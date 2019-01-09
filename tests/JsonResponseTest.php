dc0b4ceb2d086ddd4748145b0190feab4f3b80c6
Ömer Kamçılı <omerkamcili@Omer-iMac.local>
2019-01-09 17:07:00 +0300

    Unit test added, JsonResponseTest case added

---
+++ tests/JsonResponseTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SmartOver\ApiResponse\Test;
+
+use PHPUnit\Framework\TestCase;
+use SmartOver\ApiResponse\JsonResponse;
+
+/**
+ * Class JsonResponseTest
+ *
+ * @package SmartOver\ApiResponse\Test
+ */
+final class JsonResponseTest extends TestCase
+{
+    /**
+     * JsonResponseTest constructor.
+     *
+     * @param string|null $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+    }
+
+    /**
+     * Test for JsonResponse class
+     *
+     * @return  void
+     */
+    public function testRender()
+    {
+
+        $response = new JsonResponse('GNR001', 200, 'OK', ['foo' => 'bar']);
+        $rendered = $response->render();
+        $decoded = json_decode($rendered->content());
+
+        $this->assertInstanceOf('SmartOver\ApiResponse\JsonResponse', $response);
+        $this->assertSame($rendered->status(), 200);
+        $this->assertSame($decoded->code, 'GNR001');
+        $this->assertSame($decoded->message, 'OK');
+        $this->assertSame($decoded->data->foo, 'bar');
+    }
+}
